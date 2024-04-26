5d3212f30bc8c609698a44f2a810975fb65bbc42
rinnyanalia <168186545+rinnyanalia@users.noreply.github.com>
2024-04-26 18:10:10 +0700

    Add files via upload

---
+++ android.py
@@ -0,0 +1,19 @@
+
+import time
+
+print("aa            aa")
+print("aaaaaaaaaaaaaaaa")
+print("aOaaaaaaaaaaaaOa")
+print("aaOaaaaaaaaaaOaa")
+print("aa232aaaaaa232aa")
+print("aaaaaaaaaaaaaaaa")
+print("aaaaaaaaaaaaaaaa")
+print("aaaaaaaaaaaaaaaa")
+print("aaaaaaaaaaaaaaaa")
+print("aaaaaaaaaaaaaaaa")
+print("aaaaaaaaaaaaaaaa")
+print("aa            aa")
+print("aa            aa")
+pass
+print()
+int()
